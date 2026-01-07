273977279d60728ff9d22f2add89426d5f5a07c5
istiakiraz <istiakiraz@gmail.com>
2026-01-07 11:02:27 +0600

    develop responsive navbar

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,8 +1,16 @@
 "use client";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { SignInButton, useUser } from "@clerk/nextjs";
+import { X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const navLinks = [
   { name: "Home", path: "/" },
@@ -12,6 +20,14 @@ const navLinks = [
 
 function Header() {
   const { user } = useUser();
+
+  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+    setIsOpen(!open);
+  };
 
   return (
     <div className="bg-primary/5 backdrop-blur-md py-4">
@@ -26,7 +42,7 @@ function Header() {
             {/* The Logo Image */}
             <Image
               src={"/trevolaBlurLogo.png"}
-              className="w-fit relative z-10 group-hover:-translate-y-20 group-hover:translate-x-20 transition-transform duration-700 ease-in-out"
+              className="lg:w-fit w-12  relative z-10 group-hover:-translate-y-20 group-hover:translate-x-20 transition-transform duration-700 ease-in-out"
               width={60}
               height={60}
               alt="logo"
@@ -40,14 +56,18 @@ function Header() {
           </div>
 
           <div>
-            <h2 className="text-primary text-4xl font-extrabold">trevola</h2>
-            <p className="text-primary font-bold text-xs">Travel made easier</p>
+            <h2 className="text-primary text-2xl md:text-3xl  lg:text-4xl font-extrabold">
+              trevola
+            </h2>
+            <p className="text-primary font-bold text-[10px] md:text-xs">
+              Travel made easier
+            </p>
           </div>
         </Link>
 
-        {/* nav links */}
+        {/* desktop nav links */}
 
-        <div className="flex   items-center gap-6">
+        <div className="md:flex hidden  items-center gap-6">
           {navLinks.map((link, index) => (
             <div
               key={index}
@@ -67,7 +87,7 @@ function Header() {
 
         {/* get started btn + profile */}
 
-        <div>
+        <div className="md:block hidden">
           {/* <Button className="font-bold" >Get Started</Button> */}
 
           <>
@@ -96,15 +116,14 @@ function Header() {
             `}</style>
 
             {user ? (
-              <Link href={'/create-new-trip'}>
+              <Link href={"/create-new-trip"}>
                 <div className="rainbow relative z-0 bg-primary overflow-hidden p-0.5 flex items-center justify-center rounded-full hover:scale-105 transition duration-300 active:scale-100">
                   <button className="px-6 text-sm py-2 font-bold cursor-pointer text-white rounded-full  bg-primary backdrop-blur">
                     Create New Trip
                   </button>
                 </div>
               </Link>
-            ) : 
-            (
+            ) : (
               <SignInButton mode="modal">
                 <div className="rainbow relative z-0 bg-primary overflow-hidden p-0.5 flex items-center justify-center rounded-full hover:scale-105 transition duration-300 active:scale-100">
                   <button className="px-6 text-sm py-2 font-bold cursor-pointer text-white rounded-full  bg-primary backdrop-blur">
@@ -115,6 +134,86 @@ function Header() {
             )}
           </>
         </div>
+
+        {/* Mobile drawer */}
+        <Drawer
+          open={open}
+          onOpenChange={(val) => {
+            setOpen(val);
+            setIsOpen(val);
+          }}
+          direction="top"
+        >
+          <DrawerTrigger asChild>
+            <button
+              onClick={toggleDrawer}
+              type="button"
+              className="inline-flex items-center p-2 w-8 h-8 justify-center text-sm md:hidden"
+              aria-controls="navbar-sticky"
+              aria-expanded={isOpen}
+            >
+              <div className="flex flex-col items-center justify-center w-10 h-10 space-y-1.5">
+                <span
+                  className={`w-8 h-1 rounded transition-all bg-primary duration-300 ${
+                    isOpen ? "rotate-45 translate-y-2" : ""
+                  }`}
+                ></span>
+                <span
+                  className={`w-8 h-1 rounded transition-all bg-primary duration-300 ${
+                    isOpen ? "opacity-0" : ""
+                  }`}
+                ></span>
+                <span
+                  className={`w-8 h-1 rounded transition-all bg-primary duration-300 ${
+                    isOpen ? "-rotate-45 -translate-y-2" : ""
+                  }`}
+                ></span>
+              </div>
+            </button>
+          </DrawerTrigger>
+
+          <DrawerContent className="h-2/6 w-full md:hidden  bg-primary/30 backdrop-blur-md fixed left-0 top-20 rounded-none">
+            <DrawerHeader className="flex items-end justify-end">
+              <DrawerClose asChild>
+                <button className="text-white p-2">
+                  <X className="h-5 w-5" />
+                </button>
+              </DrawerClose>
+            </DrawerHeader>
+
+            <nav className="flex flex-col  justify-between h-full">
+              <div className="flex flex-col items-center text-center  p-5 pt-0 space-y-4">
+                {navLinks.map((link) => (
+                  <a
+                    key={link.path}
+                    href={link.path}
+                    className=" text-xl text-white border-b w-full text-center duration-300 ease-in-out hover:text-gray-300"
+                  >
+                    {link.name}
+                  </a>
+                ))}
+
+                {user ? (
+                  <Link href={"/create-new-trip"}>
+                    <div className="rainbow relative z-0 bg-primary my-5 overflow-hidden p-0.5 flex items-center justify-center rounded-full hover:scale-105 transition duration-300 active:scale-100">
+                      <button className="px-6 text-sm py-2 font-bold cursor-pointer text-white rounded-full  bg-primary backdrop-blur">
+                        Create New Trip
+                      </button>
+                    </div>
+                  </Link>
+                ) : (
+                  <SignInButton mode="modal">
+                    <div className="rainbow relative z-0 bg-primary overflow-hidden p-0.5 flex items-center justify-center rounded-full hover:scale-105 transition duration-300 active:scale-100">
+                      <button className="px-6 text-sm py-2 font-bold cursor-pointer text-white rounded-full  bg-primary backdrop-blur">
+                        Get Started
+                      </button>
+                    </div>
+                  </SignInButton>
+                )}
+              </div>
+            </nav>
+          </DrawerContent>
+        </Drawer>
       </div>
     </div>
   );
