3b2f100d91bba63f5fa0cdde42c5bfdda6b4fe59
istiakiraz <istiakiraz@gmail.com>
2026-01-08 11:22:18 +0600

    make ai chat box ui

--- app/create-new-trip/_components/ChatBox.tsx
+++ app/create-new-trip/_components/ChatBox.tsx
@@ -44,17 +44,23 @@ function ChatBox() {
       {/* Display Chat Messages */}
 
       <section className="flex-1 overflow-y-auto p-4">
-        <div className="flex justify-end mt-2">
-          <div className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg ">
-            User Message goes here
+
+      {messages.map((msg:Message, idx) =>(
+        msg.role === "user" ? <div key={idx} className="flex justify-end mt-2">
+          <div  className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg ">
+            {msg.content}
+          </div>
+        </div> :
+
+        <div key={idx} className="flex justify-start mt-2">
+          <div className="max-w-lg bg-gray-200 px-4 py-2 rounded-lg ">
+            {msg.content}
           </div>
         </div>
 
-        <div className="flex justify-start mt-2">
-          <div className="max-w-lg bg-gray-200 px-4 py-2 rounded-lg ">
-            Ai Agent Message goes here
-          </div>
-        </div>
+      ))}
+
+        
       </section>
 
       {/* user input */}
